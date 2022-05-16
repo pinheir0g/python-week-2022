4e0d53c2e3fd597f709c9fd263cf1382e34cfe6a
pinheir0g <gv.pinheiro@outlook.com>
2022-05-16 16:32:49 -0300

    att

--- tests/test_functional_api.py
+++ tests/test_functional_api.py
@@ -8,7 +8,7 @@ client = TestClient(api)
 
 def test_create_beer_via_api():
     response = client.post(
-        "/beers",
+        "/beers/",
         json={
             "name": "Skol",
             "style": "KornPA",
@@ -17,14 +17,14 @@ def test_create_beer_via_api():
             "cost": 2,
         },
     )
-    assert response.status_code == 201
+    assert response.status_code == 200
     result = response.json()
     assert result["name"] == "Skol"
     assert result["id"] == 1
 
 
 def test_list_beers():
-    response = client.get("/beers")
+    response = client.get("/beers/")
     assert response.status_code == 200
     result = response.json()
     assert len(result) == 0
